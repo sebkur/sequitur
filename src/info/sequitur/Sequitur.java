96e1d26ca396a2b59c1ee386de2ea835f4ce574b
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2014-06-02 09:40:56 +0200

    Moved copyright statement to the top of the files

--- src/info/sequitur/Sequitur.java
+++ src/info/sequitur/Sequitur.java
@@ -1,5 +1,3 @@
-package info.sequitur;
-
 /*
  This class is part of a Java port of Craig Nevill-Manning's Sequitur algorithm.
  Copyright (C) 1997 Eibe Frank
@@ -19,6 +17,8 @@ package info.sequitur;
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
+package info.sequitur;
 
 import java.awt.Font;
 import java.awt.GridBagConstraints;
